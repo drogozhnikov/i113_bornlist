a50b8e87739210490660cbfc7f3d22f7a5bf9462
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-05-10 10:46:28 +0300

    Added: downloadTemplate controller and functional

--- src/main/java/bornlist/controller/DataController.java
+++ src/main/java/bornlist/controller/DataController.java
@@ -3,8 +3,16 @@ package bornlist.controller;
 import bornlist.service.DataService;
 import bornlist.service.UnitService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @RestController
 //@CrossOrigin //needed to Vue
@@ -25,5 +33,23 @@ public class DataController {
         unitService.loadAndReplaceJson(username,dataService.readJson(file));
     }
 
+    @GetMapping("/template")
+    public ResponseEntity<Object> downloadTemplate() throws IOException {
+        File file = dataService.getTemplateFile();
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        ResponseEntity<Object>
+                responseEntity = ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(
+                MediaType.parseMediaType("application/txt")).body(resource);
+
+        return responseEntity;
+    }
 
 }
